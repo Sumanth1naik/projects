9d919181864a123063b602349209380d17065404
Sumanth Naik <sumanthmuttu524@gmail.com>
2023-04-19 09:44:25 +0530

    changed the models.py file

--- questions/questions/app1/models.py
+++ questions/questions/app1/models.py
@@ -8,7 +8,7 @@ class questions(models.Model):
     op2 = models.CharField(max_length=100)
     op3 = models.CharField(max_length=100)
     op4 = models.CharField(max_length=100)
-
+    ans = models.CharField(max_length=100)
     def __str__(self):
         return self.question
     
