5975ae99acd5d8d8ce945380c031f097099c39d7
Sumanth Naik <sumanthmuttu524@gmail.com>
2023-04-19 09:46:30 +0530

    null field is added

--- questions/questions/app1/models.py
+++ questions/questions/app1/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class questions(models.Model):
     question = models.CharField(max_length=2000,help_text="enter the question")
-    op1 = models.CharField(max_length=100)
-    op2 = models.CharField(max_length=100)
-    op3 = models.CharField(max_length=100)
-    op4 = models.CharField(max_length=100)
-    ans = models.CharField(max_length=100)
+    op1 = models.CharField(max_length=100 , null=True)
+    op2 = models.CharField(max_length=100,null=True)
+    op3 = models.CharField(max_length=100,null=True)
+    op4 = models.CharField(max_length=100,null=True)
+    ans = models.CharField(max_length=100,null=True)
     def __str__(self):
         return self.question
     
